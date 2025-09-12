09cd5df869703d130a5fd7535807001c77027e41
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2025-09-12 13:58:49 +0300

    Time: 15 ms (12.76%) | Memory: 30.5 MB (5.01%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,15 +1,23 @@
 import re
 class Solution(object):
     def isPalindrome(self, s):
-        strs=[]
+        st=[]
         for i in s:
-            if  i.isalnum():
-                strs.append(i.lower())
-        
-        if strs==strs[::-1]:
-            return True
-        else:
-            return False
+            if i.isalnum():
+                st.append(i.lower())
+        n=len(st)
+       
+        def  check(i,n):
+            if i>=n/2:
+                return True
+            if st[i]!=st[n-i-1]:
+                return False
+            return  check(i+1,n)
+        return  check(0,n)
+
+
+
+
    
     
         
