5f7ab0d9369503ea4997b8b2f4e6760f127c3bea
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2025-10-16 15:01:56 +0300

    Time: 0 ms (100.00%) | Memory: 14.5 MB (15.31%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,26 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums.sort()
-        count=1
-        maxss=0
-        if  len(nums)==1:
-            return nums[0]
-        for i in range(1,len(nums)):
-            if nums[i-1]==nums[i]:
+        count=0
+        
+        for i in range(len(nums)):
+            if count==0:
+                count=1
+                el=nums[i]
+            elif nums[i]==el:
                 count+=1
             else:
-                maxss=max(maxss,count)
-                count=1
-            if  len(nums)%2==1:
-                if  count>=(len(nums)/2)+1:
-                    return nums[i]
-            else:
-                if  count>=len(nums)/2:
-                    return nums[i]
-               
-
-            
+                count-=1
+        return el
            
           
                 
