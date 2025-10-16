e6833b1ed65f3938e868db2e23d857ea2dc0f90c
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2025-10-16 11:50:02 +0300

    Time: 0 ms (100.00%) | Memory: 12.6 MB (11.53%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,8 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        countzero=0
+        countone=0
+        counttwo=0
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]>nums[j]:
-                    nums[i],nums[j]=nums[j],nums[i]
+            if nums[i]==0:
+                countzero+=1
+            elif nums[i]==1:
+                countone+=1
+            else:
+                counttwo+=1
+        for i in range(countzero):
+            nums[i]=0
+        s=countone+countzero
+        h=s+counttwo
+        for  i in range(countzero,s):
+            nums[i]=1
+        for i in range(s,h):
+            nums[i]=2
         return nums
+       
+       
+
+           
