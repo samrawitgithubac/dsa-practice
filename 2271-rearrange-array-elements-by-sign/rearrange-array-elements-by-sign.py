9e1bd63b73d682a39c37be790e330b7717ffb302
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2025-10-19 15:19:10 +0300

    Time: 71 ms (74.08%) | Memory: 48 MB (20.06%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -4,35 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        arry1=[]
-        arry2=[]
-       
-        i=0
-        for i in range(len(nums)):
-            if nums[i]>0:
-                arry1.append(nums[i])
+        arr=[0]*len(nums)
+        postiveindex=0
+        negativeindex=1
+        for  i in range(len(nums)):
+            if nums[i]<0:
+                arr[negativeindex]=nums[i]
+                negativeindex+=2
+            
             else:
-                arry2.append(nums[i])
-        j=0
-        for i in range(len(arry1)):
-            if j%2==0:
-                nums[j]=arry1[i]
-                j+=2
-        j=1
-        for i in range(len(arry2)):
-            if j%2==1:
-                nums[j]=arry2[i]
-                j+=2
-        
-            
-        return nums
-               
-               
-          
+                arr[postiveindex]=nums[i]
+                postiveindex+=2
+        return arr
 
-
-        
-        
-
-            
-            
