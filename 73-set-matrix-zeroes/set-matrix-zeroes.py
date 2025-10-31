a469f258adb1a5e4e4272dbbfc3fe1d5a8c6d50c
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2025-10-31 15:54:54 +0300

    Time: 7 ms (45.99%) | Memory: 13 MB (98.00%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -5,18 +5,34 @@ class Solution(object):
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
         ans=[]
-        col=[0]*len(matrix[0])
-        row=[0]*len(matrix)
+        col0=1
+       
         for  i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j]==0:
-                    col[j]=1
-                    row[i]=1
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if (row[i])or(col[j])==1:
+                    matrix[i][0]=0
+                    if j!=0:
+                        matrix[0][j]=0
+                    else:
+                        col0=0
+        
+
+        for i in range(1,len(matrix)):
+            for j in range(1,len(matrix[0])):
+                if matrix[0][j]==0 or matrix[i][0]==0:
                     matrix[i][j]=0
+        if matrix[0][0]==0:
+            for j in range(len(matrix[0])):
+                matrix[0][j]=0
+        if col0==0:
+            for i in range(len(matrix)):
+                matrix[i][0]=0
         return matrix
+           
+
+       
+
+       
                 
 
 
