78c2490bff9e68760e80d2a37e92ae5b58f7e698
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2025-11-12 06:12:23 +0300

    Time: 0 ms (100.00%) | Memory: 12.4 MB (53.94%) - LeetSync

---
+++ 31-next-permutation/next-permutation.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        # Step 1: Find the first decreasing element from the right
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        
+        # Step 2: If found, find the smallest element greater than nums[i] to the right
+        if i >= 0:
+            j = len(nums) - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        
+        # Step 3: Reverse the elements to the right of i
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
