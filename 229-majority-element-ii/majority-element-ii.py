2a3e625de75d5a8831addb59f06f7c7d56c0fec5
samrawit <149234089+samrawitgithubac@users.noreply.github.com>
2026-01-01 06:34:09 +0300

    Time: 7 ms (63.21%) | Memory: 19.8 MB (96.17%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,15 +1,34 @@
-from collections import  Counter
 class Solution:
-    def majorityElement(self, nums: List[int]) -> List[int]:
-        nums_counters=Counter(nums)
-        
-        res=[]
-        for i in  nums_counters:
-            if nums_counters[i]>len(nums)/3:
-                res.append(i)
-        return res
+    def majorityElement(self, nums):
+        if not nums:
+            return []
 
-               
+        # Step 1: Find candidates
+        candidate1, candidate2 = None, None
+        count1, count2 = 0, 0
 
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                candidate1 = num
+                count1 = 1
+            elif count2 == 0:
+                candidate2 = num
+                count2 = 1
+            else:
+                count1 -= 1
+                count2 -= 1
 
-            
+        # Step 2: Verify candidates
+        result = []
+        n = len(nums)
+
+        if nums.count(candidate1) > n // 3:
+            result.append(candidate1)
+        if candidate2 is not None and nums.count(candidate2) > n // 3:
+            result.append(candidate2)
+
+        return result
